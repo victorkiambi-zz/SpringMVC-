7084751a2e028fd5af1ac52215448743d7f284f6
victorkiambi <kiambi15@gmail.com>
2018-12-10 21:46:53 +0300

    spring rest api

---
+++ demo/src/main/java/payroll/Employee.java
@@ -0,0 +1,25 @@
+package payroll;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+class Employee {
+
+    public Employee() {
+
+    }
+
+    private @Id @GeneratedValue Long id;
+    private String name;
+    private String role;
+
+ public Employee(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+}
---
+++ demo/src/main/java/payroll/EmployeeNotFoundException.java
@@ -0,0 +1,7 @@
+package payroll;
+
+public class EmployeeNotFoundException extends RuntimeException {
+    public EmployeeNotFoundException(Long id) {
+        super("Could not find employee " + id);
+    }
+}
